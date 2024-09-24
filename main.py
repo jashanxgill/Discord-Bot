90d47a36eebacdbcd426d9f1b8ca1d91de5d5eef
Jashan Gill <jashangill3218@gmail.com>
2024-09-24 08:26:56 +0530

    Update main.py
    The Bot has been brought online now and a simple command !hello has been added for testing and confirming the basic functionality

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot {bot.user.name} has connected to Discord!')
+
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+bot.run('token')
 
