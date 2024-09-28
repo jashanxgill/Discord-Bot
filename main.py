8830cdcaca7f725407ecc8a7706393f5ea59f5b2
Jashan Gill <jashangill3218@gmail.com>
2024-09-28 16:50:15 +0530

    Update main.py
    Added Variable for Prefix

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 import discord
 from discord.ext import commands
 
+prefix = "!"
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 
 @bot.event
 async def on_ready():
